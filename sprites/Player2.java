20f34377038fbf2bb424bd5e799185269a6e2079
owenp500 <opringle9@gmail.com>
2022-12-22 14:28:14 -0700

    commit

--- sprites/Player2.java
+++ sprites/Player2.java
@@ -11,6 +11,8 @@ public class Player2 extends Player {
 		super(centerX, centerY);
 		//Player2 does not invoke the setKeys method as the default keys are meant for player 2
 		//although if you want to change the keys than feel free to use the method here
+		super.setFacingRight(false);
+		
 	}
 	
 	
