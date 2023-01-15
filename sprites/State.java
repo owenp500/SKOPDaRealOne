0a9d55e889b1b115eae514fc6988ef9f5eff8d99
Saniru <saniruk1@educbe.ca>
2023-01-14 22:01:08 -0700

    sprites

--- sprites/State.java
+++ sprites/State.java
@@ -1,3 +1,5 @@
+
+// Idle(0); Move(1); Defend(2); LowIdle(3); LowBlock(4); LowAttack(5); Stun(6); Attack(7)
 public enum State { IDLE(0), MOVE(1), DEFEND(2), LOW_IDLE(3), ATTACK(7), STUN(6);
 	public int value = 0;
 	private State(int value) {
