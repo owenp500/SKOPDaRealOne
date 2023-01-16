8791289c68506ce336c6c4ce603abb8666000337
Saniru <saniruk1@educbe.ca>
2023-01-15 20:35:15 -0700

    commit

--- sprites/Player3.java
+++ sprites/Player3.java
@@ -13,12 +13,22 @@ public class Player3 extends Player {
 		//assigns whether the player is 'player1' or 'player2' or 'AI' which is 'player3'
 		super.setPlayer(3);
 		
-		@Override
-		public void update(Universe universe, KeyboardInput keyboard, long actual_delta_time) {
-			
-		}
+		class AI {
+		    private int state;
 
-		
-		
-	}
+		    public AI() {
+		        state = 0;
+		    }
+
+		    public void chooseDirection() {
+		        if (state == 0) {
+		            System.out.println("Going Left");
+		            state = 1;
+		        } else {
+		            System.out.println("Going Right");
+		            state = 0;
+		        }
+		    }
+		}
+}
 }
